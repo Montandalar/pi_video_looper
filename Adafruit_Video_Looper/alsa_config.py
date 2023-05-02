87dda7bd672b5bebad0443d1099f79f4a03d541a
Jason Bigelow <jbis1337@hotmail.com>
2023-05-02 14:44:22 +1000

    Implement a search by ALSA device name
    This is a fallback so that if a hwid is not found by regex, a name
    search will be performed instead to try to find a hwid.

--- Adafruit_Video_Looper/alsa_config.py
+++ Adafruit_Video_Looper/alsa_config.py
@@ -1,4 +1,12 @@
 import re
+import pyalsa.alsacard
+
+# Return a device ID (hwid)
+def parse_device_name(searchName):
+    for cardNo in pyalsa.alsacard.card_list():
+        cardName = pyalsa.alsacard.card_get_name(cardNo)
+        if cardName == searchName:
+            return cardNo
 
 def parse_hw_device(s):
     if not s:
@@ -7,6 +15,8 @@ def parse_hw_device(s):
     m = re.match("^(\d+),(\d+)$", s)
 
     if not m:
-        raise RuntimeError('Invalid value for alsa hardware device: {}'.format(s))
+        m = parse_device_name(s)
+        if not m:
+            raise RuntimeError('Invalid value for alsa hardware device: {}'.format(s))
     
     return tuple(map(int, m.group(1, 2)))
